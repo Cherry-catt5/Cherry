5ef9f828929d548513f59966257dd9718195d394
Cherry-catt5 <nekoma1369@gmail.com>
2025-08-21 10:27:22 -0500

    Cherry clase 2

---
+++ asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplication {
+    public static void main(String[] args) {
+        System.out.println("Iniciando asistencia CHERRY");
+        AsistenciaApplication.main(args);
+    }
+}
