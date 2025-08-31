2e7cb82993f5b74eb2ba8ca388229de084a7a1ae
Cherry-catt5 <nekoma1369@gmail.com>
2025-08-31 12:00:11 -0500

    Cherry

---
+++ ConceptosPOO/src/main/java/pe/edu/upeu/vistacontrol/Indexprincipal.java
@@ -0,0 +1,35 @@
+package pe.edu.upeu.vistacontrol;
+
+import pe.edu.upeu.utils.Lectura;
+import pe.edu.upeu.utils.Utiliarios;
+
+public class Indexprincipal {
+    private static Lectura leer = new Lectura();
+
+    public static void menu() {
+        System.out.print("""
+                *** MENU PRINCIPAL ***
+                1. Trabajador
+                2. Estudiante
+                3. Salir
+                """);
+
+        System.out.print("Seleccione opcion [1-3]: ");
+    }
+
+    public static void inicio() {
+        int opcion;
+        Indextrabajador.datosdeinstalacion();
+        Indexestudiante.datosdeinstalacion();
+        do {
+            menu();
+            opcion = leer.entero();
+            switch (opcion) {
+                case 1 -> Indextrabajador.inicio();
+                case 2 -> Indexestudiante.inicio();
+                case 3 -> Utiliarios.salir();
+                default -> Utiliarios.error(1);
+            }
+        } while (opcion != 3);
+    }
+}
